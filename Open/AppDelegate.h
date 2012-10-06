0671394a86001f3c78c946edb66d24e9382c5cd9
David Holdeman <david@davids-mac-pro.westell.com>
2012-10-06 10:10:53 -0500

    commit 1

--- Open/AppDelegate.h
+++ Open/AppDelegate.h
@@ -3,12 +3,18 @@
 //  Open
 //
 //  Created by David Holdeman on 10/4/12.
-//  Copyright (c) 2012 David Holdeman. All rights reserved.
+//  Copyright (c) 2012 ChuckWagon Comuputing. All rights reserved.
 //
 
 #import <Cocoa/Cocoa.h>
 
 @interface AppDelegate : NSObject <NSApplicationDelegate>
+{
+    
+    IBOutlet NSMenu *openMenu;
+    NSStatusItem *statusItem;
+    IBOutlet NSWindow *editWindow;
+}
 
 @property (assign) IBOutlet NSWindow *window;
 
--- Open/AppDelegate.m
+++ Open/AppDelegate.m
@@ -3,16 +3,25 @@
 //  Open
 //
 //  Created by David Holdeman on 10/4/12.
-//  Copyright (c) 2012 David Holdeman. All rights reserved.
+//  Copyright (c) 2012 ChuckWagon Computing. All rights reserved.
 //
 
 #import "AppDelegate.h"
 
 @implementation AppDelegate
 
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
-{
-    // Insert code here to initialize your application
+
+@synthesize window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    [NSApp activateIgnoringOtherApps:YES];
+}
+
+-(void)awakeFromNib{
+    statusItem = [[[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength] retain];
+    [statusItem setMenu:openMenu];
+    [statusItem setTitle:@"O"];
+    [statusItem setHighlightMode:YES];
 }
 
 @end
